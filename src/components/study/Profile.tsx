1d298997de6ff9669ed0f60f8ecfc93db4e003eb
yunnb <chldbsrud010@naver.com>
2025-04-02 09:40:30 +0900

    study

---
+++ src/components/study/Profile.tsx
@@ -0,0 +1,22 @@
+
+interface Props {
+    name: string;
+    job: string;
+}
+
+export default function Profile({name, job}: Props) {
+    return (
+        <div>
+            <h1>프로필</h1>
+            <div>
+                <b>이름: </b>
+                {name}
+            </div>
+            <div>
+                <b>직업: </b>
+                {job}
+            </div>
+        </div>
+    )
+
+}
