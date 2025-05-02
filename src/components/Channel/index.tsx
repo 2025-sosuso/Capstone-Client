a337e370846cf794b62a0804139122dcf5f53831
yunnb <chldbsrud010@naver.com>
2025-05-02 22:00:51 +0900

    feat: Video 컴포넌트 생성 및 유튜브 영상 기능 구현

--- src/components/Channel/index.tsx
+++ src/components/Channel/index.tsx
@@ -1,14 +1,17 @@
-'use client'; // YouTubePlayer나 내부에서 useState 쓰는 경우 필요
+'use client';
 
 import Channel from '@/components/Channel/Channel';
-import YouTubePlayer from '@components/youtube/YouTubePlayer';
+import Video from "@components/Video/Video";
+import {mockVideoData} from "@mock/video";
+
 
 const Index = () => {
     return (
         <div className="flex flex-col w-full px-7 py-5 gap-4 rounded-3xl bg-gray-100">
             <h1 className="text-2xl font-semibold text-gray-900">🌟 관심 채널의 최근 영상</h1>
             <Channel />
-            <YouTubePlayer />
+            <Video video={mockVideoData} />
+            {/*<YouTubePlayer />*/}
         </div>
     );
 };
