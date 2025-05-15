5a631b84ee5ca7fd025303385888d34ba5dd46cd
yunnb <chldbsrud010@naver.com>
2025-05-15 23:51:05 +0900

    refactor: Thumbnail 컴포넌트를 props 기반으로 개선

--- src/components/home/Thumbnail.tsx
+++ src/components/home/Thumbnail.tsx
@@ -1,14 +1,7 @@
-import {mockVideoData} from "@mock/video";
-
-export default function Thumbnail() {
+export default function Thumbnail({ src }: { src: string }) {
     return (
         <div className="w-full max-w-[360px] aspect-video rounded-2xl overflow-hidden">
-            <img
-                src={mockVideoData[0].thumbnailUrl}
-                className="w-full h-full object-cover"
-                alt="썸네일"
-            />
+            <img src={src} className="w-full h-full object-cover" alt="썸네일" />
         </div>
     );
 }
-
--- src/components/home/subscribed-channels/index.tsx
+++ src/components/home/subscribed-channels/index.tsx
@@ -2,13 +2,14 @@
 
 import ChannelAvatarList from '@components/home/subscribed-channels/ChannelAvatarList';
 import Thumbnail from "@components/home/Thumbnail";
+import {mockVideoData} from "@mock/video";
 
 export default function SubscribedChannels() {
     return (
         <div className="flex flex-col w-full px-7 py-5 gap-4 rounded-3xl bg-gray-100">
             <h1 className="text-2xl font-semibold text-gray-900">🌟 관심 채널의 최근 영상</h1>
             <ChannelAvatarList/>
-            <Thumbnail/>
+            <Thumbnail src={mockVideoData[0].thumbnailUrl}/>
         </div>
     );
 };
