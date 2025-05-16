66e7bac7f5c4a5fee0b47276a886114f2c24d2ed
yunnb <chldbsrud010@naver.com>
2025-05-16 22:11:51 +0900

    refactor: HeaderSearch heroicons 적용 방식 변경

--- src/components/layout/header/HeaderSearch.tsx
+++ src/components/layout/header/HeaderSearch.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import {MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 
 export default function HeaderSearch() {
     const [search, setSearch] = useState('');
@@ -30,17 +31,7 @@ export default function HeaderSearch() {
                 onClick={handleSearch}
                 className="pl-3 pr-4 py-2 hover:bg-gray-100 transition-colors duration-200 rounded-r-full cursor-pointer"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5 text-gray-400"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                >
-                    <path strokeLinecap="round" strokeLinejoin="round"
-                          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
-                </svg>
+                <MagnifyingGlassIcon className="size-5 text-gray-400 stroke-2" />
             </div>
         </div>
     );
