19b623530e2a9b15054f9033d1edc9afaf64c2e6
yunnb <chldbsrud010@naver.com>
2025-05-26 22:44:22 +0900

    feat: 상세페이지 하단 레이아웃 구성

--- src/components/detail/index.tsx
+++ src/components/detail/index.tsx
@@ -1,9 +1,23 @@
 import VideoInfo from "@components/detail/video-info";
+import { mockVideoResponse } from "@/mock/video.mock";
 
-export default function Detail(){
+export default function Detail() {
+    // mock data
+    const videoItem = mockVideoResponse.videoInfo.items[0];
+    const comments = mockVideoResponse.commentInfo.allComments;
+
     return (
-        <div className="flex w-full items-center">
-            <VideoInfo/>
+        <div className="flex flex-col w-full items-center gap-10">
+            <VideoInfo item={videoItem} />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-6">
+                <div className="col-auto">
+                   {/*왼쪽*/}
+                </div>
+                <div className="col-auto">
+                    {/*오른쪽*/}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
