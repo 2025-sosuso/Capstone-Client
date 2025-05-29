f4011ebbf4e990c26cee24764066f4730027763e
yunnb <chldbsrud010@naver.com>
2025-05-29 21:17:47 +0900

    feat: 댓글 및 분석 컴포넌트 추가

--- src/components/detail/index.tsx
+++ src/components/detail/index.tsx
@@ -1,5 +1,11 @@
 import VideoInfo from "@components/detail/video-info";
-import { mockVideoResponse } from "@/mock/video.mock";
+import SectionLayout from "@components/detail/SectionLayout";
+import CommentList from "@components/detail/CommentList";
+import LanguageChart from "@components/detail/LanguageChart";
+import CommentTimeChart from "@components/detail/CommentTimeChart";
+import TagList from "@components/detail/TagList";
+import AISummary from "@components/detail/AISummary";
+import {mockVideoData} from "@mock/new-video-mock";
 
 export default function Detail() {
     // mock data
@@ -16,12 +22,33 @@ export default function Detail() {
         <div className="flex flex-col w-full items-center max-w-[1000px] m-auto gap-10">
             <VideoInfo data={mockData.video}/>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-6">
-                <div className="col-auto">
-                   {/*왼쪽*/}
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-10">
+                <div className="col-auto flex flex-col gap-10">
+                    <SectionLayout header="AI 전체 요약">
+                        <AISummary summary={aiSummary} />
+                    </SectionLayout>
+                    <SectionLayout header="좋아요 Top 5">
+                        <CommentList comments={commentsTop5}/>
+                    </SectionLayout>
+                    <SectionLayout header="전체 댓글 확인하기">
+                        <CommentList comments={commentsAll}/>
+                    </SectionLayout>
                 </div>
-                <div className="col-auto">
-                    {/*오른쪽*/}
+
+                <div className="col-auto flex flex-col gap-10">
+                    <SectionLayout header="언어 비율">
+                        <LanguageChart data={languageRatio}/>
+                    </SectionLayout>
+                    <SectionLayout header="가장 많이 언급한 시간대">
+                        <TagList tags={popularTimeTags}/>
+                    </SectionLayout>
+                    <SectionLayout header="댓글 작성 시간대">
+                        <CommentTimeChart data={commentTimes}/>
+                    </SectionLayout>
+                    <SectionLayout header="키워드 분석">
+                        <TagList tags={keywordTags} />
+                        <CommentList comments={commentsAll} />
+                    </SectionLayout>
                 </div>
             </div>
         </div>
