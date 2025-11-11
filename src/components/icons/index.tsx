0141817609f776ed4aaeb8191a7dac183d43a766
yunnb <chldbsrud010@naver.com>
2025-11-11 11:50:22 +0900

    chore: 아이콘 추가

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -6,6 +6,8 @@ import HeartSvg from '@/assets/icons/heart.svg';
 import NotificationSvg from '@/assets/icons/notification.svg';
 import TriangleDownSvg from '@/assets/icons/triangle-down.svg';
 import TriangleUpSvg from '@/assets/icons/triangle-up.svg';
+import ConfirmSvg from '@/assets/icons/confirm.svg';
+import CancelSvg from '@/assets/icons/cancel.svg';
 
 const getIconStyle = (size?: number | string) => {
     if (!size) return {};
@@ -38,5 +40,7 @@ export const HeartIcon = createIcon(HeartSvg, 'text-red-400');
 export const NotificationIcon = createIcon(NotificationSvg, 'text-gray-600');
 export const TriangleDownIcon = createIcon(TriangleDownSvg);
 export const TriangleUpIcon = createIcon(TriangleUpSvg);
+export const ConfirmIcon = createIcon(ConfirmSvg, 'text-white');
+export const CancelIcon = createIcon(CancelSvg, 'text-white');
 
 export type {IconProps};
